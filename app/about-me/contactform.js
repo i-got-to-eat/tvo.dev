573ec2335a3bcbacb90e7e94040729783feec5b7
Tam Vo <khaitamvo@gmail.com>
2023-09-12 23:35:08 +0000

    Added Form Validation

--- app/about-me/contactform.js
+++ app/about-me/contactform.js
@@ -1,52 +1,28 @@
 'use client';
 
-import { useReducer, useState } from 'react';
-
-
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 export default function ContactForm() {
 
-	const INITIAL_FORM_STATE = {
-		loading: false,
-		error: [],
-	}
+	const validationSchema = Yup.object().shape({
+		firstName:          Yup.string().max(255, "Your First Name is too long").required("First Name is required"),
+		lastName:           Yup.string().max(255, "Your Last Name is too long").required("Last Name is required"),
+		email:              Yup.string().max(255, "Your Email is too long").required("Email is required").email("Email is invalid"),
+		phoneNumber:        Yup.string().max(50, "Your Phone Number is too long").required("Phone Number is required"),
+		companyName:        Yup.string().max(255, "Your Company/Affiliate Name is too long").required("Company/Affiliate Name is required"),
+		message:            Yup.string().max(16_777_215, "Your Message is too long").required("Message is required"),
+	});
+	const formOptions = { resolver: yupResolver(validationSchema) };
+	const { register, handleSubmit, reset, formState } = useForm(formOptions);
+	const { errors } = formState;
 
-	const formReducer = (state, action) => {
-
-		switch(action.type){
-			case "POST_START":
-				return {
-					...state,
-					loading: true,
-					error: []
-				};
-			case "POST_SUCCESS":
-				return {
-					...state,
-					loading: false,
-					error: []
-				};
-
-			case "POST_ERROR":
-				return {
-					...state,
-					loading: false,
-					error: action.payload
-				}
-			default:
-				return state;
-		}
-	}
-
-
-	const [formState, formDispatch] = useReducer(formReducer, INITIAL_FORM_STATE)
 
 	async function onSubmit(event){
 		event.preventDefault();
 		const formData = new FormData(event.target);
 		const value = Object.fromEntries(formData.entries());
-
-		formDispatch({type:"POST_START"});
 
 		const response = await fetch('/api/submit-contact-info', {
 			method: 'POST',
@@ -56,42 +32,31 @@ export default function ContactForm() {
 			body: JSON.stringify(value),
 		});
 
-		const data = await response;
-		switch(data.status){
-			case 200:
-				formDispatch({type:"POST_SUCCESS"});
-				break;
-			case 400:
-				formDispatch({type:"POST_ERROR"}, data.body);
-				break;
-			case 500:
-				fromDispatch({type:"POST_ERROR"}, data.status);
-				break;
-
-			default:
-				fromDispatch({type:"POST_ERROR"}, "Unusual Error");
-		}
+		const data = await response.json();
 	}
 	return (
 		<div className="max-w-full">
 		<h2 className="grid grid-rows-1 place-content-center font-semibold text-4xl text-slate-50/80 my-5">Contact Me</h2>
-		<form onSubmit={onSubmit} className="grid auto-rows-max place-content-center text-slate-100/90 font-medium text-xl pb-10">
-			<label for="first-name" className="font-medium">First Name:</label> 
-			<input id="first-name" type="text" name="First Name" autoComplete="given-name" className="mb-4 p-1 text-black"/>
+		<form onSubmit={handleSubmit(onSubmit)} className="grid auto-rows-max place-content-center text-slate-100/90 font-medium text-xl pb-10">
+	
+			<label for="first-name" className="font-medium">First Name:</label>
+			<span id="first-name-feedback" aria-label="polite">{errors.firstName?.message}</span>
+			<input {...register('firstName')} id="first-name" type="text" name="First Name" autoComplete="given-name" required aria-required="true" className="mb-4 p-1 text-black focus:border-sky-500 focus:ring-1 focus:ring-sky-500"/>
+	
 				
 			<label for="last-name">Last Name:</label> 
-			<input id="last-name" type="text" name="Last Name" autoComplete="family-name" className="mb-4 p-1 text-black"/>
-
+			<span id="last-name-feedback" aria-label="polite">{errors.lastName?.message}</span>
+			<input {...register('lastName')} id="last-name" type="text" name="Last Name" autoComplete="family-name" required aria-required="true" className="mb-4 p-1 text-black focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500"/>
 			<label for="email">Email:</label> 
-			<input id="email" type="email" name="Email" autoComplete="email" className="mb-4 p-1 text-black"/>
-			
+			<span id="email-name-feedback" aria-label="polite">{errors.email?.message}</span>
+			<input {...register('email')} id="email" type="email" name="Email" autoComplete="email" required aria-required="true" className="mb-4 p-1 text-black focus:border-sky-500 focus:ring-1 focus:ring-sky-500 invalid:border-pink-500"/>	
 			<label for="phone-number">Phone Number:</label> 
-			<input id="phone-number" type="tel" name="Phone Number" autoComplete="tel" className="mb-4 p-1 text-black"/>
+			<span id="phone-number-feedback" aria-label="polite">{errors.phoneNumber?.message}</span>
+			<input {...register('phoneNumber')}id="phone-number" type="tel" name="Phone Number" autoComplete="tel" required aria-required="true" className="mb-4 p-1 text-black"/>
 
 			<label for="company">Company/Affiliate:</label>
-			<input id="company" type="text" name="Company" autoComplete="organization" className="mb-4 p-1 text-black"/>
-
-
+			<span id="company-name-feedback" aria-label="polite">{errors.companyName?.message}</span>
+			<input {...register('companyName')} id="company" type="text" name="Company" autoComplete="organization" required aria-required="true" className="mb-4 p-1 text-black"/>
 
 			<label for="email-type">How would you like to work with me?</label>
 			<select id="email-type" name="Email Type" className="mb-4 text-black">
@@ -101,8 +66,10 @@ export default function ContactForm() {
 			<option value="SPONSORSHIP">Sponsorship</option>
 			</select>
 
+
 			<label for="message">Message:</label> 
-			<textarea id="message" type="text" name="Message" autoComplete="off" className="mb-4 h-96 text-black px-2 py-1"/>
+			<span id="company-name-feedback" aria-label="polite">{errors.message?.message}</span>
+			<textarea {...register('message')} id="message" type="text" name="Message" autoComplete="off" required aria-required="true" className="mb-4 h-96 text-black px-2 py-1"/>
 
 			<button type="submit" className="py-3 px-5 border-solid rounded border-2 text-slate-100 border-slate-500 bg-slate-400/50 hover:rounded-md">Submit</button>
 		</form>
